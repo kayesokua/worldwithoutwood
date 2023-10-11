8ba6ec9596c6dd3b4907ada1c61854e0fa8fcf5b
Kaye <kaye.sokua@code.berlin>
2023-10-11 10:22:12 +0200

    final

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-
 @app.route('/')
 def index():
     catalog = [
@@ -12,7 +11,7 @@ def index():
             'image_url': 'static/assets/images/ashni-ePWaAwUn80k-unsplash.jpg',
             'title': 'Paper',
             'content': 'Wood has been used to make paper for centuries, but it became more common in the 19th century with the invention of the wood pulp papermaking process.',
-            'source': ''
+            'source': 'https://www.tis-gdv.de/tis/ware/papier/zeitung/zeitung-htm/'
         },
         {
             'image_url': 'https://lyocell.info/images/lyocell-outfit.jpeg',
@@ -27,39 +26,31 @@ def index():
             'source': 'University of Maryland Energy Research Center'
         },
         {
-            'image_url': 'assets/images/shutterstock-1369448567.jpg',
-            'title': 'Lightweight Cars',
-            'content': '',
-            'source': ''
-        },
-        {
-            'image_url': 'assets/images/shutterstock-1369448567.jpg',
+            'image_url': 'https://www.tugraz.at/fileadmin/user_upload/tugrazExternal/8c980dc9-7c6f-4a5b-b72f-e7450816135d/images/Abbildungen/Concept.png',
             'title': 'Battery Housings',
-            'content': 'Wood has been used to make paper for centuries...',
-            'source': ''
+            'content': 'BioLIB is a project aiming to enhance electric vehicle battery housings by combining wood and steel to achieve better temperature management, crash performance, vibration damping, thermal containment, cost-effectiveness, and a reduced ecological footprint.',
+            'source': 'https://www.tugraz.at/projekte/biolib/home/'
         },
         {
-            'image_url': 'assets/images/shutterstock-1369448567.jpg',
+            'image_url': 'https://images.unsplash.com/photo-1638028928694-2e683c5278a7?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2980&q=80',
             'title': 'Xylitol (Gum)',
-            'content': 'BioLIB is a project aiming to enhance electric vehicle battery housings by combining wood and steel to achieve better temperature management, crash performance, vibration damping, thermal containment, cost-effectiveness, and a reduced ecological footprint.',
-            'source': 'https://www.tugraz.at/projekte/biolib/home/'
+            'content': 'Xylitol, also known as birch sugar, is produced through a chemical transformation of sugar alcohols.',
+            'source': 'www.sciencedirect.com'
         },
         {
-            'image_url':'https://source.unsplash.com/random/?wood',
-            'title': 'Laminated Veneer Lumber',
-            'content': 'BioLIB is a project aiming to enhance electric vehicle battery housings by combining wood and steel to achieve better temperature management, crash performance, vibration damping, thermal containment, cost-effectiveness, and a reduced ecological footprint.',
-            'source': 'https://www.tugraz.at/projekte/biolib/home/'
-        },
-        {
-            'image_url':'https://source.unsplash.com/random/?wood',
+            'image_url':'https://source.unsplash.com/random/?computer,chips',
             'title': 'Computer Chips',
             'content': 'These chips work just like traditional chips, but they use a different material for their base layer. Instead of silicon, they use cellulose nanofibril (CNF), a flexible and biodegradable material made from wood. This means that the chips can decompose in nature, which is a big improvement over traditional chips, which can take hundreds of years to break down.',
             'source': 'https://www.nature.com/articles/ncomms8170'
         },
+        {
+            'image_url':'https://en.wikipedia.org/wiki/File:Laminated_Veneer_Lumber.png',
+            'title': 'Laminated Veneer Lumber',
+            'content': 'Laminated veneer lumber (LVL) is significant in construction due to its superior strength and stability, which makes it a versatile and reliable material for various structural applications. Additionally, LVL use of smaller, fast-growing trees and efficient manufacturing processes contributes to sustainability and reduces the demand for large timber resources.',
+            'source': 'steico.com'
+        }
 
-        
-
-        
+   
     ]
     return render_template('index.html', utc_dt=datetime.datetime.utcnow(), catalog=catalog)
 
@@ -91,11 +82,7 @@ def credits():
             "media": "International Journal of Environmental Science and Technology",
             "url": "https://link.springer.com/article/10.1007/s13762-017-1282-6 "
         },
-        {
-            "title": "The benefits of modern method of construction based on wood in the context of sustainability",
-            "media": "International Journal of Environmental Science and Technology",
-            "url": "https://www.researchgate.net/profile/Elias-Hurmekoski-2/publication/291358292_Long-term_outlook_for_wood_construction_in_Europe/links/56a529cb08ae232fb207978e/Long-term-outlook-for-wood-construction-in-Europe.pdf"
-        },
+       
         {
             "title": "Circular economy in wood construction",
             "media": "Construction and Building Materials",
@@ -106,16 +93,7 @@ def credits():
             "media": "International Journal of Environmental Science and Technology",
             "url": "https://www.nature.com/articles/ncomms8170"
         },
-        {
-            "title": "High-performance green flexible electronics based on biodegradable cellulose nanofibril paper",
-            "media": "International Journal of Environmental Science and Technology",
-            "url": "https://www.nature.com/articles/ncomms8170"
-        },
-        {
-            "title": "High-performance green flexible electronics based on biodegradable cellulose nanofibril paper",
-            "media": "International Journal of Environmental Science and Technology",
-            "url": "https://www.nature.com/articles/ncomms8170"
-        },
+        
         {
             "title":  "Facts about Paper",
             "media": "f-mp.de",
@@ -126,5 +104,19 @@ def credits():
             "media": "tis-gdv.de",
             "url": "https://www.tis-gdv.de/tis/ware/papier/zeitung/zeitung-htm/"
         },
+        {
+            "title": "Market share of Wood",
+            "media": "Statista.com",
+            "url": "https://www.statista.com/outlook/io/manufacturing/material-products/wood/worldwide"
+        },
+        {
+            "title": "STEICO LVL laminated veneer lumber",
+            "media": "STEICO LVL",
+            "url": "https://www.steico.com/en/solutions/product-advantages/lvl-laminated-veneer-lumber"
+        },
+
+
+        
+
     ]
     return render_template('credits.html', utc_dt=datetime.datetime.utcnow(), title="Credits", resources=resources)
